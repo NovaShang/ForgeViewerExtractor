732bc39b8bb5bcb3e6a47c0f466d710cb1ae12cc
NovaShang <styleshang@yeah.net>
2017-06-21 17:48:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const Vue = require('./node_modules/vue/dist/vue.js');
+
+
+const app = new Vue({
+    el: "#app",
+    data: {
+
+    }
+})
---
+++ task.js
@@ -0,0 +1,17 @@
+const ForgeSDK = require('forge-apis');
+
+module.exports = function() {
+    var api = new ForgeSDK.BucketsApi();
+    api.createBucket()
+}
+
+
+/**
+ * @module asdfasdf
+ * 
+ * @param asdfsadfsdaf
+ */
+
+module.exports = function() {
+
+}
