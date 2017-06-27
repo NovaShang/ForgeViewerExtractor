5dfb02b2cf186584282df9caf042f0450ea54350
NovaShang <styleshang@yeah.net>
2017-06-27 12:26:50 +0800

    a

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ const app = new Vue({
         taskQueue: [],
         currentTask: {},
         advanceMode: false
-
-
+    },
+    created: function() {
+        this.setAdvanceMode(false)
     },
     methods: {
+        setAdvanceMode: function(value) {
+            let currentSize = currentWindow.getSize();
+            if (value) {
+                currentWindow.setMinimumSize(600, 400);
+                currentWindow.setMaximumSize(99999, 99999);
+                currentWindow.setSize(1000, currentSize[1])
+
+
+            } else {
+                currentWindow.unmaximize();
+                currentWindow.setMinimumSize(500, 400);
+                currentWindow.setMaximumSize(500, 99999);
+                currentWindow.setSize(500, currentSize[1]);
+            }
+            this.advanceMode = value;
+        },
         min: function() {
             currentWindow.minimize();
 
